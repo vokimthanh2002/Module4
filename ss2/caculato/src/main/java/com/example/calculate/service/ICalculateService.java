37a8f04422acbe522ccde634d5397b7ea65625f8
vokimthanh2002 <vothanh6081@gmail.com>
2023-03-07 23:28:14 +0700

    update ss2 calculate

---
+++ ss2/caculato/src/main/java/com/example/calculate/service/ICalculateService.java
@@ -0,0 +1,8 @@
+package com.example.calculate.service;
+
+public interface ICalculateService {
+    float plusExpression(float num1, float num2);
+    float minusExpression(float num1, float num2);
+    float multiplicationExpression(float num1, float num2);
+    float divisionExpression(float num1, float num2);
+}
